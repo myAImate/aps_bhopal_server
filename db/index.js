bbd59f6266888b50c01e1eeb12833a3b963701db
frenzycoder29 <frenzycoder29@gmail.com>
2021-04-26 17:21:52 +0530

    modified db file

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const local="mongodb://127.0.0.1:27017/assessment"
 const online ='mongodb+srv://myaimateuser:myaimateuser@tutorial.bb9zo.mongodb.net/tutorial?retryWrites=true&w=majority'
 mongoose
-    .connect(local, { 
+    .connect(online, { 
         useNewUrlParser: true,
         useCreateIndex:true,
         useUnifiedTopology:true 
